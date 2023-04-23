ca83b9afcb15918ebaa8ca92a42a8c46108d658d
Hannah <115044113+hannahnobeebux@users.noreply.github.com>
2023-04-23 22:16:12 +0100

    demo using a docker-compose.yml file to deploy application

--- sequelize.js
+++ sequelize.js
@@ -3,11 +3,18 @@
 const path = require("path");
 const { DataTypes, Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-	dialect: "sqlite",
-	logging: process.env.NODE_ENV !== "production" && console.log,
-	storage: path.join(__dirname, "db.sqlite"),
-});
+const {MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST} = process.env; 
+const sequelize = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, {
+	dialect:"mysql", 
+	host: MYSQL_HOST, 
+	logging: process.env.NODE_ENV !== "production" && console.log
+})
+
+// const sequelize = new Sequelize({
+// 	dialect: "sqlite",
+// 	logging: process.env.NODE_ENV !== "production" && console.log,
+// 	storage: path.join(__dirname, "db.sqlite"),
+// });
 
 const Produce = sequelize.define("Produce", {
 	name: DataTypes.STRING,
